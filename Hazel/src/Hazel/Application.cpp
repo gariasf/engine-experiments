eaee8a094041edb97734c3ec51eb8898192fb5b3
Guillem Arias Fauste <hello@gariasf.com>
2024-06-25 13:11:57 +0200

    setup basic Application and Entry Point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,13 @@
+#include "Application.h"
+
+namespace Hazel {
+	Application::Application()
+	{
+	}
+	Application::~Application()
+	{
+	}
+	void Application::Application::Run() {
+		while (true);
+	}
+}
